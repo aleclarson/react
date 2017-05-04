985bcd27eca5f190fe2be645e7ffe6e43ae9c19b
aleclarson <alec.stanford.larson@gmail.com>
2017-05-04 00:36:12 -0400

    Prefer calling `ReactBrowserEventEmitter.listenTo` with `containerInfo._node` instead of `_ownerDocument`
    We would rather listen to events on the container element, instead of the document.

--- src/renderers/dom/shared/ReactDOMComponent.js
+++ src/renderers/dom/shared/ReactDOMComponent.js
@@ -216,8 +216,7 @@ function enqueuePutListener(inst, registrationName, listener, transaction) {
     );
   }
   var containerInfo = inst._hostContainerInfo;
-  var isDocumentFragment = containerInfo._node && containerInfo._node.nodeType === DOC_FRAGMENT_TYPE;
-  var doc = isDocumentFragment ? containerInfo._node : containerInfo._ownerDocument;
+  var doc = containerInfo._node || containerInfo._ownerDocument;
   listenTo(registrationName, doc);
   transaction.getReactMountReady().enqueue(putListener, {
     inst: inst,
